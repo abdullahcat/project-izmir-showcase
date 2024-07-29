1b63b82f04cba229deac26be51d9d1075b118635
abdullahcat <abd190307.0@gmail.com>
2024-07-29 03:32:55 +0300

    just need to add how to download

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -37,24 +37,22 @@ function Hero() {
 
     return (
         <section  >
-            <div className="bg-warmneutral">
-                <div className="container flex flex-col items-center px-4 pt-20 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-black">
-                    <img src="/project-izmir-assets/logo/project-logo.png" className="h-16 w-16" alt="" />
+            <div className="container h-screen flex flex-col items-center px-4 pt-20 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-black">
+                <img src="/project-izmir-assets/logo/project-logo.png" className="h-16 w-16" alt="" />
 
-                    <h1 className="text-5xl  mt-3    font-black">&quot;<span className='text-red-500'>{text}</span>&quot;</h1>
-                    <p className="my-6 text-lg   xl:max-w-3xl  ">
-                        Kiraların, mülklerin, giderlerin artık tek bir yerde.
-                    </p>
-                    <div className="flex flex-wrap justify-center gap-2">
-                        <Link
-                            href={'http://projectizmir.artesdeilusion.com'}
-                            className="relative inline-block px-5 py-2 border bg-red-500 border-red-500 text-white hover:-translate-y-1   rounded-full space-x-2 flex flex-row   hover:border-red-600 transition duration-300 ease-in-out hover:bg-red-600"
-                        >
-                            Başla
-                        </Link>
+                <h1 className="text-5xl mt-3 " >&quot;<span className='text-red-500'>{text}</span>&quot;</h1>
+                <p className="my-6 text-lg xl:max-w-3xl  ">
+                    Kiraların, mülklerin, giderlerin artık tek bir yerde.
+                </p>
+                <div className="flex flex-wrap justify-center gap-2">
+                    <Link
+                        href={'http://projectizmir.artesdeilusion.com'}
+                        className="relative inline-block px-5 py-2 border bg-red-500 border-red-500 text-white hover:-translate-y-1   rounded-full space-x-2 flex flex-row   hover:border-red-600 transition duration-300 ease-in-out hover:bg-red-600"
+                    >
+                        Başla
+                    </Link>
 
 
-                    </div>
                 </div>
             </div>
             <img src="/project-izmir-assets/general/project-properties-on-mac.png" alt="" className="w-6/7 mx-auto pb-10  px-4 -mt-20 rounded-lg lg:-mt-40" />
